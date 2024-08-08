3b5b12fe418fbe598b3cff459de874fa3ebbc38c
Anurag Yadav <yanurag994@gmail.com>
2024-08-08 00:58:56 -0400

    Adding Automated ManyLinux Build to GIthub Artifact

--- pyLHF/script.sh
+++ pyLHF/script.sh
@@ -7,6 +7,6 @@ cp -f /lhf/build/LHFmain/libLHFlib.so /lhf/pyLHF/src/lhf/
 cd /lhf/pyLHF/src/
 "/opt/python/cp310-cp310/bin/pip3" wheel --no-deps -w wheelhouse/ . --no-cache
 
-cd /lhf/pyLHF/src/wheelhouse
-auditwheel show "lhf-2.0.1-py3-none-any.whl"
-auditwheel repair "lhf-2.0.1-py3-none-any.whl" --plat "manylinux_2_24_x86_64"
+auditwheel show "wheelhouse/lhf-2.0.1-py3-none-any.whl"
+auditwheel repair "wheelhouse/lhf-2.0.1-py3-none-any.whl" --plat "manylinux_2_24_x86_64"
+chmod -R 777 /lhf/pyLHF/src/wheelhouse
